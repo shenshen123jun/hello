ba2a687e1aeebed9d63c811ae1c298515eadfe8f
yanshen <shenshen123jun@163.com>
2015-04-19 17:25:21 +0800

    world

---
+++ src/main/java/com/yanshen/World.java
@@ -0,0 +1,12 @@
+package com.yanshen;
+/** 
+ * @author yanshen  
+ * @version 创建时间：2015年4月19日 
+ * 类说明 
+ */
+public class World {
+
+	public static void main(String[] args) {
+		System.out.println("world");
+	}
+}
