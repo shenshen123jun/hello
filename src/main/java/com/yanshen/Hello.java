94f4affc04207a6aa511b8442ae6da71c574572c
yanshen <shenshen123jun@163.com>
2015-04-19 17:24:06 +0800

    初始化

---
+++ src/main/java/com/yanshen/Hello.java
@@ -0,0 +1,11 @@
+package com.yanshen;
+/** 
+ * @author yanshen  
+ * @version 创建时间：2015年4月19日 
+ * 类说明 
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("yanshen");
+	}
+}
